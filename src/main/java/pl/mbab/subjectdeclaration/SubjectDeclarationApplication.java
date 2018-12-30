16fa6303d003daf798d1be49002ba7191410a199
Marcel Babski <dindane@o2.pl>
2018-12-30 21:55:19 +0100

    initial commit

---
+++ src/main/java/pl/mbab/subjectdeclaration/SubjectDeclarationApplication.java
@@ -0,0 +1,14 @@
+package pl.mbab.subjectdeclaration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectDeclarationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectDeclarationApplication.class, args);
+    }
+
+}
+
